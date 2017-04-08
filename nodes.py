7e4a2365ea3ebe53dd41c186d09fbef3574cacff
Elliot Hallmark <permafacture@gmail.com>
2017-04-08 00:22:00 -0500

    Added Node class. print random numbers

--- nodes.py
+++ nodes.py
@@ -1,14 +1,38 @@
-def writer():
-    """A coroutine that writes data *sent* to it to fd, socket, etc."""
+from time import sleep
+from random import random
+
+class Node():
+
+    def __init__(self, name, readers=[], writers=[]):
+        self.name = name
+        self.readers = readers if readers is not None else []
+        self.writers = writers if writers is not None else []
+
+    def decide_to_write(self):
+        while True:
+            w = random()
+            sleep(w)
+            self.write(w)
+
+    def write(self, w):
+        for reader in self.readers:
+            reader.send(w)
+
+def reader():
     while True:
-        w = (yield)
-        print('>> ', w)
+        r = (yield)
+        print(r)
 
-def writer_wrapper(coro):
-    yield from coro
+r = reader()
+r.send(None)
 
-w = writer()
-wrap = writer_wrapper(w)
-wrap.send(None)  # prine the coroutine
-for i in range(4):
-    wrap.send(i)
+node = Node('bob', [r])
+
+for _ in node.decide_to_write():
+    pass
+
+# w = writer()
+# wrap = writer_wrapper(w)
+# wrap.send(None)  # prine the coroutine
+# for i in range(4):
+#     wrap.send(i)
