a9311c99ad3845f9a2c7fbbef5d41508edb1e73f
ryanfmurphy <ryanfmurphy@gmail.com>
2017-04-27 23:38:59 -0500

    working proof-of-concept websocket connect
    between JS client and python3 server

---
+++ websocket_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    while True:
+        name = await websocket.recv()
+        print("< {}".format(name))
+
+        greeting = "Hello {}!".format(name)
+        await websocket.send(greeting)
+        print("> {}".format(greeting))
+
+start_server = websockets.serve(hello, 'localhost', 9876)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
